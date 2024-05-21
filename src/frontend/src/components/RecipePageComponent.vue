cd7923d24445f80d8f8cdb95e4d03d1bfdc9887e
Veronika Atanasova <g7l1812@gmail.com>
2024-05-22 01:29:21 +0300

    Design improvements, add displaying recipe likes

--- src/frontend/src/components/RecipePageComponent.vue
+++ src/frontend/src/components/RecipePageComponent.vue
@@ -8,9 +8,20 @@
              class="recipe-image"/>
         <img v-else :src="require(`../../../main/resources/images/defaultRecipe.png`)"
              alt="default recipe picture" class="recipe-image">
+        <div class="like-container">
+          <div v-if="loggedIn">
+            <i class="far fa-heart fa-3x" id="like" v-if="showLike" @click="likeRecipe"></i>
+            <i class="fas fa-heart fa-3x" id="unlike" v-else @click="unlikeRecipe" style="color: #cd7979"></i>
+          </div>
+          <div v-if="likes" class="likes">
+            <i class="fas fa-heart" style="margin-right: 0;"></i>
+            {{ likes }}
+          </div>
+        </div>
       </div>
       <div v-if="recipe" class="details">
-        <router-link class="edit-link" v-if="showEdit" :to="'/recipe/edit/' + recipe.id"><i class="fas fa-edit"></i>edit</router-link>
+        <router-link class="edit-link" v-if="showEdit" :to="'/recipe/edit/' + recipe.id"><i class="fas fa-edit"></i>edit
+        </router-link>
         <div class="title">
           <h2 class="title">{{ recipe.title }}
             <MDBBadge v-if="recipe.aiGenerated" color="primary">AI</MDBBadge>
@@ -18,7 +29,7 @@
         </div>
         <div v-if="recipe.recipeTime" class="detail-item">
           <i class="fas fa-clock"></i>
-           {{ recipe.recipeTime }}
+          {{ recipe.recipeTime }}
         </div>
         <div v-if="recipe.recipeCalories" class="detail-item">
           <i class="fas fa-utensils"></i>
@@ -37,27 +48,25 @@
         <div class="ingredients">
           <strong>Ingredients:</strong>
           <div class="toggle-switch">
-            <label>
-              Display in
-              <select v-model="displayMode">
-                <option value="weight">Weight</option>
-                <option value="volume">Volume</option>
-              </select>
-            </label>
+            <div class="switch-container">
+              <p style="display: inline-block; margin-right: 10px;">Volume</p>
+              <MDBSwitch class="custom-control-input" id="measurementSwitch" v-model="isWeightMode"
+                         @change="toggleDisplayMode"></MDBSwitch>
+              <p style="display: inline-block; margin-left: 10px;">Weight</p>
+            </div>
           </div>
-          <ul class="ingredient-list">
-            <li v-for="(ingredientItem, index) in recipe.recipeIngredients" :key="index" class="ingredient">
-              <i class="fas fa-cookie"></i> {{ ingredientItem.ingredient.ingredient }} {{ displayIngredientValue(ingredientItem) }}
-            </li>
-          </ul>
+          <table class="ingredients-table">
+            <tbody>
+            <tr v-for="(ingredientItem, index) in recipe.recipeIngredients" :key="index">
+              <td><i class="fas fa-cookie"></i> {{ ingredientItem.ingredient.ingredient }}</td>
+              <td>{{ displayIngredientValue(ingredientItem) }}</td>
+            </tr>
+            </tbody>
+          </table>
         </div>
         <div class="instructions">
           <strong>Instructions:</strong>
           <pre>{{ recipe.instructions }}</pre>
-        </div>
-        <div v-if="loggedIn">
-          <button v-if="showLike" @click="likeRecipe">Like</button>
-          <button v-else @click="unlikeRecipe">Unlike</button>
         </div>
       </div>
     </div>
@@ -71,7 +80,7 @@
 
 <script>
 import axios from 'axios';
-import {MDBBadge} from "mdb-vue-ui-kit";
+import {MDBBadge, MDBSwitch} from "mdb-vue-ui-kit";
 import axiosInstance from "@/components/apiClient";
 import {MDBSpinner} from "mdb-vue-ui-kit";
 
@@ -79,7 +88,8 @@ export default {
   name: 'RecipePageComponent',
   components: {
     MDBBadge,
-    MDBSpinner
+    MDBSpinner,
+    MDBSwitch,
   },
   data() {
     return {
@@ -94,6 +104,7 @@ export default {
       loggedIn: true,
       showEdit: false,
       imageUrl: '',
+      likes: '',
     };
   },
   mounted() {
@@ -101,6 +112,7 @@ export default {
     this.isLoggedIn();
     if (this.loggedIn)
       this.checkLike();
+    this.getLikesCount();
   },
   computed: {
     profileLink() {
@@ -138,11 +150,15 @@ export default {
             this.recipe = response.data;
             // Set default display mode based on recipe defaultMetric
             this.displayMode = this.recipe.defaultMetric === 'volume' ? 'volume' : 'weight';
+            this.isWeightMode = this.displayMode === 'weight';
           })
           .catch(error => {
             console.error('Error fetching recipe details:', error);
           });
       await this.checkCurrentUser();
+    },
+    toggleDisplayMode() {
+      this.displayMode = this.isWeightMode ? 'volume' : 'weight';
     },
     displayIngredientValue(ingredientItem) {
       return this.displayMode === 'weight' ? ingredientItem.weight : ingredientItem.volume;
@@ -156,6 +172,7 @@ export default {
       axiosInstance.post(`/like`, {"recipeId": this.$route.params.recipeId, "userId": localStorage.getItem('id')})
           .then(() => {
             this.showLike = false;
+            this.getLikesCount();
           })
           .catch(error => {
             console.error('Error for like:', error);
@@ -170,6 +187,7 @@ export default {
       })
           .then(() => {
             this.showLike = true;
+            this.getLikesCount()
           })
           .catch(error => {
             console.error('Error for unlike:', error);
@@ -190,6 +208,17 @@ export default {
             console.error('Error:', error);
           });
     },
+    getLikesCount() {
+      axios.get(`/api/public/likes?recipeId=${this.$route.params.recipeId}`)
+          .then(response => {
+            if (response.data) {
+              this.likes = response.data;
+            }
+          })
+          .catch(error => {
+            console.error('Error:', error);
+          });
+    },
   },
 };
 </script>
@@ -197,7 +226,7 @@ export default {
 <style scoped>
 .recipe-container {
   display: flex; /* Use flexbox layout */
-  background-color: rgba(225, 250, 227, 0.76);
+  background-color: #f9f9f9;
   max-width: 80%;
   padding: 3%;
   margin: auto; /* Center align the container */
@@ -218,9 +247,17 @@ export default {
   box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
 }
 
+.like-container {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 10px;
+  margin-top: 10px;
+}
+
 .details {
   font-size: large;
-  font-family: "Arial",serif;
+  font-family: "Arial", serif;
   flex: 2; /* Let the details take up more space */
   padding-left: 20px;
 }
@@ -242,13 +279,31 @@ export default {
   margin-bottom: 20px;
 }
 
-.ingredient-list {
-  list-style-type: none;
-  padding: 0;
+.ingredients {
+  margin-top: 20px;
 }
 
-.ingredient {
+.toggle-switch {
+  margin-top: 10px;
   margin-bottom: 10px;
+}
+
+.ingredients-table {
+  width: 50%;
+  border-collapse: collapse;
+  margin-left: 25%;
+  text-align: left;
+}
+
+.ingredients-table td {
+  padding: 5px 10px;
+}
+
+.instructions {
+  margin-top: 20px;
+  width: 80%;
+  margin-left: 10%;
+  text-align: justify;
 }
 
 pre {
@@ -261,5 +316,13 @@ pre {
   right: 20px;
   text-decoration: none;
   color: #007bff;
+}
+
+#like:hover, #unlike:hover {
+  transform: translateY(-5px);
+}
+
+i {
+  margin-right: 5px;
 }
 </style>
